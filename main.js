f8e01412605d53e9aa16c56891313bc4c9bfff58
Mohcine Boujraf <mohssine441@gmail.com>
2023-07-07 14:14:28 +0200

    v4.Console Methods And Styling And WebAPI

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 window.onload = function(){
     //document.querySelector("h1").style.color="red"
-    window.alert("Hello From JS File");
-    document.write("<h1>Hello <span> Page</span></h1>");
-    console.log("Hello from JS")
+    //window.alert("Hello From JS File");
+    //document.write("<h1>Hello <span> Page</span></h1>");
+    //console.log("Hello from JS")
+    
+    //v3:
+    //console.table(["Mohcine","Peter","Jan"]);
+    //console.log("Hello From JS %cFile","color: red; font-size: 40px");
     
 };
 
