5bf48b7e2804c1403fc29eacfafd4e54236b4c2d
Mohcine Boujraf <mohssine441@gmail.com>
2023-07-07 14:02:48 +0200

    v3. Output To Screen

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 window.onload = function(){
-    document.querySelector("h1").style.color="red"
+    //document.querySelector("h1").style.color="red"
+    window.alert("Hello From JS File");
+    document.write("<h1>Hello <span> Page</span></h1>");
+    console.log("Hello from JS")
+    
 };
 
