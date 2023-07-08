14f28076133a05b3892a67d255840ae3a1bc889b
Mohcine Boujraf <mohssine441@gmail.com>
2023-07-08 09:36:19 +0200

    v13.Var, Let, Const Compare

--- main.js
+++ main.js
@@ -8,6 +8,21 @@ window.onload = function(){
     //console.table(["Mohcine","Peter","Jan"]);
     //console.log("Hello From JS %cFile","color: red; font-size: 40px");
     
+    //----------v13 Var, Let, Const Compare----------:
+
+    //Redeclare:
+    //var a = 1;
+    //var a = 2;
     
+    //Variable Scope Drama [Added To Window]:
+    //var aabb = 1;
+    // in console you can access the variable with window.aabb or aabb
+    //let aacc = 1;
+    // in console you can access the variable only with aacc
+
+
+
+
+
 };
 
